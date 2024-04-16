1da671a899b40c854ea9e4ca75c5c53ea173bc7d
Nnad6840 <nandkishordhadhal121@gmail.com>
2024-04-16 13:31:25 +0530

    auth0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-sc8lywc7rb0kor6k.us.auth0.com"
      clientId="ESiHWzQCXGyuIsyZmUheCeC2zgIWEHag"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://pitch-hub.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
