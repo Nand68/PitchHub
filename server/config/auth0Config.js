b14309564ffb456895591c3975616fa8b036d099
Nnad6840 <nandkishordhadhal121@gmail.com>
2024-04-16 16:01:30 +0530

    chnage

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://pitch-hub-server.vercel.app",
     issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
